c18d80a25a19cab0d1492b5574fa76b6a71c4864
y-tiant <64068155+y-tiant@users.noreply.github.com>
2020-04-22 11:15:53 +0800

    Update User.java

--- src/main/java/cn/jiyun/pojo/User.java
+++ src/main/java/cn/jiyun/pojo/User.java
@@ -2,11 +2,11 @@ package cn.jiyun.pojo;
 
 public class User {
     private String password;
-
+    private int age;
     public String getPassword() {
         return password;
     }
-
+    
     public void setPassword(String password) {
         this.password = password;
     }
