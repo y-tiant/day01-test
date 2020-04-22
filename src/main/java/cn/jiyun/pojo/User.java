1eeffca750836fba35393e43f0da3b1f1747c6ab
y-tiant <1847946473@qq.com>
2020-04-22 11:09:00 +0800

    提交项目

--- src/main/java/cn/jiyun/pojo/User.java
+++ src/main/java/cn/jiyun/pojo/User.java
@@ -1,6 +1,15 @@
 package cn.jiyun.pojo;
 
 public class User {
+    private String password;
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 
     private String userName;
 
